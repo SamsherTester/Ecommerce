53f8ba81fa120e83abfa60ee432d340338f8a7d1
samsher <samsher@DESKTOP-I4T556R>
2020-12-07 06:39:48 +0530

    initial commit

---
+++ Ecommerce/src/test/java/com/logintest/LoginTest.java
@@ -0,0 +1,18 @@
+package com.logintest;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	@Test
+	public void positveTest() {
+
+		System.out.println("LoginTest.positveTest()");
+	}
+
+	@Test
+	public void NegTest() {
+		System.out.println("LoginTest.NegTest()");
+
+	}
+}
